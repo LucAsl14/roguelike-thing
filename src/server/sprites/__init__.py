e3fbaacfaca4255c238717b412203a4ede650e1e
DaNubCoding <danubcoding@gmail.com>
2025-04-25 21:59:59 -0400

    add comment in server/sprites/__init__.py

--- src/server/sprites/__init__.py
+++ src/server/sprites/__init__.py
@@ -1,6 +1,8 @@
 from .player import Player
 
 from shared.shared import SharedTemplate
+# This is basically a mapping from the string type that the client receive from
+# the server TO the class of the sprite the client should use to represent it.
 sprite_classes: dict[str, type[SharedTemplate]] = {
     "Player": Player,
 }
